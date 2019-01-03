24b589f8f77ed5d560259b7c3db45f4292357a1a
servicenowcmf <31532240+servicenowcmf@users.noreply.github.com>
2019-01-03 16:04:00 -0500

    shell script to install website

---
+++ installwebsite.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#install Apache Web Server on Ubuntu 16 LTS
+#update repositories on server
+sudo apt-get -y update
+#install git
+sudo apt-get -y install git-core
+#install apache webserver
+sudo apt-get -y install apache2
+#set permissions on the html folder so files can be copied
+sudo chmod 777 /var/www/html/
+#download website files from git repository
+sudo git clone https://github.com/servicenowcmf/website
+#copy the downloaded content to the Apache www directory
+sudo cp -a ~/website/. /var/www/html/
+#change the folder to the webserver directory
+cd /var/www/html 
+#restart the webserver",
+sudo systemctl start apache2.service
