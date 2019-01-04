57b8b6fd252172acc929ee598af3be861c046abc
servicenowcmf <31532240+servicenowcmf@users.noreply.github.com>
2019-01-04 14:39:01 -0500

    plain apache webserver for Azure

---
+++ installubuntuapache.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#install Apache Web Server on Ubuntu 16 LTS
+#update repositories on server
+sudo apt-get -y update
+#install apache webserver
+sudo apt-get -y install apache2
+#restart the webserver",
+sudo systemctl start apache2.service
