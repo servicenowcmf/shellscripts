759fd4a26078230625991ce3350d5b5bc17d171b
servicenowcmf <31532240+servicenowcmf@users.noreply.github.com>
2019-01-04 15:20:23 -0500

    changed the shell used by the script

--- installubuntuapache.sh
+++ installubuntuapache.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh -e
 #install Apache Web Server on Ubuntu 16 LTS
 #update repositories on server
 sudo apt-get -y update
 #install apache webserver
 sudo apt-get -y install apache2
-#restart the webserver",
+#restart the webserver
 sudo systemctl start apache2.service
