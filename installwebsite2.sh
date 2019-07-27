d4ed1378748500fd8cd4b216abf5addf23289a63
Donnie Hamlett <donnie.hamlett@servicenow.com>
2019-07-27 03:40:40 -0400

    fixed shell script target directories

--- installwebsite2.sh
+++ installwebsite2.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+ggit#!/bin/sh -e
 #install Apache Web Server on Ubuntu 16 LTS
 #update repositories on server
 sudo apt-get -y update
@@ -11,8 +11,8 @@ sudo chmod 777 var/www/html/
 #download website files from git repository
 sudo git clone https://github.com/servicenowcmf/website2
 #copy the downloaded content to the Apache www directory
-mv /website/*.* /var/www/html
-mv /website/css /var/www/html
-mv /website/img /var/www/html
+mv /website2/*.* /var/www/html
+mv /website2/css /var/www/html
+mv /website2/img /var/www/html
 #restart the webserver",
 sudo systemctl start apache2.service
